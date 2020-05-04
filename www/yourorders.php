f52f6468f24e52758c98259ed6d99e4287893c53
ShakthiMudaliar <42892261+ShakthiMudaliar@users.noreply.github.com>
2020-05-04 19:12:35 +0530

    Update yourorders.php

--- www/yourorders.php
+++ www/yourorders.php
@@ -16,9 +16,9 @@
         <?php
             
             
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
+            $servername = "db";
+            $username = "user";
+            $password = "test";
             $dbname = "mobile";
 
             $conn = mysqli_connect($servername, $username, $password, $dbname);
