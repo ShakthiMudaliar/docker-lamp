98588bc41ff7b8a5fc8ad2716b773cdfe1be3706
ShakthiMudaliar <42892261+ShakthiMudaliar@users.noreply.github.com>
2020-05-04 18:50:15 +0530

    Update UpdateDeleteUser.php

--- www/UpdateDeleteUser.php
+++ www/UpdateDeleteUser.php
@@ -15,9 +15,9 @@ session_start();
 		$Address = clean_input($_POST["Address"]);
         $Contact = clean_input($_POST["Contact"]);
         
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
+        $servername = "db";
+        $username = "user";
+        $password = "test";
         $dbname = "mobile";
 
         $conn = mysqli_connect($servername, $username, $password, $dbname);
