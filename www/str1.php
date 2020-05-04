22190dbbbf82c3c1803e9c7d72c0b19ab04dc708
ShakthiMudaliar <42892261+ShakthiMudaliar@users.noreply.github.com>
2020-05-04 18:39:26 +0530

    Update str1.php

--- www/str1.php
+++ www/str1.php
@@ -50,9 +50,9 @@
 
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "db";
+$username = "user";
+$password = "test";
 $dbname = "mobile";
 echo "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'>";
  echo " <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>";
